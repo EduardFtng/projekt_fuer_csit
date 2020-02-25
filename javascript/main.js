1b440dd8d6b5192e722f24b4d865051d33c50af6
Ed <eduard.f1991@googlemail.com>
2020-02-25 22:00:50 +0100

    finished zeigeFilter() function

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ var minimalism = document.getElementById("minimalism");
 var micro = document.getElementById("micro");
 
 
-// Bestimmte HTML Elementen beim Laden verstecken
+// Bestimmte HTML Elementen beim Laden verstecken (index.html)
 window.onload = function () {
     dark.style.display = 'none';
     space.style.display = 'none';
@@ -13,16 +13,46 @@ window.onload = function () {
     micro.style.display = 'none';
 }
 
-//HTML Elemente ein- und ausblenden
+//HTML Elemente ein- und ausblenden (index.html)
 function zeigeFilter(id) {
-    if (neuste.style.display === "none") {
+    if (id == neuste) {
         neuste.style.display = "flex";
-    } else {
-        neuste.style.display = "none";
+        dark.style.display = 'none';
+        space.style.display = 'none';
+        minimalism.style.display = 'none';
+        micro.style.display = 'none';
+    }
+    else if (id == dark) {
+        neuste.style.display = 'none';
+        dark.style.display = 'flex';
+        space.style.display = "none";
+        minimalism.style.display = 'none';
+        micro.style.display = 'none';
+    }
+    else if (id == space) {
+        neuste.style.display = 'none';
+        dark.style.display = 'none';
+        space.style.display = "flex";
+        minimalism.style.display = 'none';
+        micro.style.display = 'none';
+    }
+    else if (id == minimalism) {
+        neuste.style.display = 'none';
+        dark.style.display = 'none';
+        space.style.display = "none";
+        minimalism.style.display = 'flex';
+        micro.style.display = 'none';
+    }
+    else {
+        neuste.style.display = 'none';
+        dark.style.display = 'none';
+        space.style.display = "none";
+        minimalism.style.display = 'none';
+        micro.style.display = 'flex';
     }
 }
 
-// Funktion um Elemente anzuzeigen
+// Funktion um Elemente anzuzeigen (index.html)
 function zeigeAlles() {
     neuste.style.display = "flex";
     dark.style.display = "flex";
