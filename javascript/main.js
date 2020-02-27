a37ab02a5de9110c5573d4afb3076887b1d2023f
Ed <eduard.f1991@googlemail.com>
2020-02-27 13:28:16 +0100

    added Impressum modal (.html, .css and .js)

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,3 @@
-var neuste = document.getElementById("neuste");
-var dark = document.getElementById("dark");
-var space = document.getElementById("space");
-var minimalism = document.getElementById("minimalism");
-var micro = document.getElementById("micro");
-
-
 // Bestimmte HTML Elementen beim Laden verstecken (index.html)
 window.onload = function () {
     dark.style.display = 'none';
@@ -12,6 +5,13 @@ window.onload = function () {
     minimalism.style.display = 'none';
     micro.style.display = 'none';
 }
+
+var neuste = document.getElementById("neuste");
+var dark = document.getElementById("dark");
+var space = document.getElementById("space");
+var minimalism = document.getElementById("minimalism");
+var micro = document.getElementById("micro");
+
 
 //HTML Elemente ein- und ausblenden (index.html)
 function zeigeFilter(id) {
@@ -21,29 +21,25 @@ function zeigeFilter(id) {
         space.style.display = 'none';
         minimalism.style.display = 'none';
         micro.style.display = 'none';
-    }
-    else if (id == dark) {
+    } else if (id == dark) {
         neuste.style.display = 'none';
         dark.style.display = 'flex';
         space.style.display = "none";
         minimalism.style.display = 'none';
         micro.style.display = 'none';
-    }
-    else if (id == space) {
+    } else if (id == space) {
         neuste.style.display = 'none';
         dark.style.display = 'none';
         space.style.display = "flex";
         minimalism.style.display = 'none';
         micro.style.display = 'none';
-    }
-    else if (id == minimalism) {
+    } else if (id == minimalism) {
         neuste.style.display = 'none';
         dark.style.display = 'none';
         space.style.display = "none";
         minimalism.style.display = 'flex';
         micro.style.display = 'none';
-    }
-    else {
+    } else {
         neuste.style.display = 'none';
         dark.style.display = 'none';
         space.style.display = "none";
@@ -64,4 +60,31 @@ function zeigeAlles() {
 // Alert Fenster für die Navigationsleiste
 function alertWindow() {
     alert("Funktion wird bald implementiert!");
+}
+
+// JS für Modal
+// Getter für Modal
+var modal = document.getElementById("impressumModal");
+
+// Getter für button um Modal einzubleden
+var btn = document.getElementById("impressumBtnId");
+
+// Getter für <span> Element bzw. X um Modal zu schließen
+var span = document.getElementsByClassName("close")[0];
+
+// Funktion um den Modal einzublenden
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// Funktion um den Modal, durch den X, zu schließen
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Funktion um den Modal, durch ein Click außerhalt von Modal, zu schließen
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
